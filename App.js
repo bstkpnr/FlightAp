4f3c305ec92a6f02595f283099bb4f0a6204b4a8
Beste Tokpınar <bstkpnr01@gmail.com>
2024-02-02 14:05:33 +0300

    edit fix

--- App.js
+++ App.js
@@ -1,26 +1,47 @@
-import { SafeAreaView, StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ScrollView,
+} from "react-native";
 import SearchForm from "./src/components/SearchForm";
 import { LinearGradient } from "expo-linear-gradient";
 import data from "./data.json";
 import FlighOptionItem from "./src/components/FlighOptionItem";
 import { useState } from "react";
-
+import StoryView from "./src/components/Story/StoryView";
+import stories from "./src/components/Story/data";
+import StoryHeader from "./src/components/Story/StoryHeader";
 const option1 = data[1];
 export default function App() {
+  const [items, setItems] = useState([]);
 
-  const [items,setItems]=useState([])
+  const onSearch = (data) => {
+    console.log(data);
 
-const onSearch=(data)=>{
-  console.log(data)
-
-  setItems(data)
-
-}
-
-
+    setItems(data);
+  };
+  const [isViewerVisible, setIsViewerVisible] = useState(false);
+  const [selectedStory, setSelectedStory] = useState(null);
+  const handleStoryPress = (story) => {
+    setSelectedStory(story);
+    setIsViewerVisible(true);
+  };
+  const closeViewer = () => {
+    setIsViewerVisible(false);
+    setSelectedStory(null);
+  };
   return (
     <LinearGradient style={styles.container} colors={["white", "#F2BE22"]}>
       <SafeAreaView>
+        <StoryHeader stories={stories} onStoryPress={handleStoryPress} />
+        <StoryView
+          story={selectedStory}
+          visible={isViewerVisible}
+          onClose={closeViewer}
+        />
         <SearchForm onSearch={onSearch} />
         <FlatList
           data={data}
